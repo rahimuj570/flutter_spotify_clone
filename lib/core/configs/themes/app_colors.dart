447a61bb6a3a5ee3f30578f877c4ec9d3f28c203
rahimuj570 <rahimuj570@gmail.com>
2026-02-21 12:14:18 +0600

    common colors using in figma tmplate is introduced in the project

---
+++ lib/core/configs/themes/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/animation.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF62CD5D);
+  static const Color lightBackgroundColor = Color(0xFFF2F2F2);
+  static const Color lightNavBarColor = Color(0xFFFFFFFF);
+  static const Color lightNavBarIconColor = Color(0xFF808080);
+  static const Color lightTextColor = Color(0xFFDBDBDB);
+  static const Color lightHintTextColor = Color(0xFF383838);
+  static const Color darkBackgroundColor = Color(0xFF0D0C0C);
+  static const Color darkNavBarColor = Color(0xFF343434);
+  static const Color darkNavBarIconColor = Color(0xFF737373);
+  static const Color grayTextOfCategoryTab = Color(0xFFBEBEBE);
+  static const Color darkTextColor = Color(0xFFE1E1E1);
+  static const Color darkHintTextColor = Color(0xFFA7A7A7);
+}
