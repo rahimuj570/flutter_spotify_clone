e77aacd719247911e0ff0ebeda9ef2a76ed5737c
rahimuj570 <rahimuj570@gmail.com>
2026-02-21 14:49:04 +0600

    splash_page ui is ready

--- lib/core/configs/app.dart
+++ lib/core/configs/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spotify_clone/core/configs/themes/app_theme.dart';
+import 'package:flutter_spotify_clone/presentation/pages/splash/splash_page.dart';
 
 class FlutterSpotifyClone extends StatefulWidget {
   const FlutterSpotifyClone({super.key});
@@ -11,6 +12,10 @@ class FlutterSpotifyClone extends StatefulWidget {
 class _FlutterSpotifyCloneState extends State<FlutterSpotifyClone> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: AppTheme.lightTheme(), home: Placeholder());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme(),
+      home: SplashPage(),
+    );
   }
 }
---
+++ lib/presentation/pages/splash/splash_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spotify_clone/core/configs/assets/app_vectors.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: SvgPicture.asset(AppVectors.logoPath)));
+  }
+}
