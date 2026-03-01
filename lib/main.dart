ed8c4addc2dc83b24e411612baa8090518c592f0
rahimuj570 <rahimuj570@gmail.com>
2026-03-01 16:00:02 +0600

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spotify_clone/core/configs/app.dart';
+import 'package:flutter_spotify_clone/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const FlutterSpotifyClone());
 }
